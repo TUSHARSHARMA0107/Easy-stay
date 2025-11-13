a0c37a2dcb8f1591ef07791677b54d6cc7f5f0cc
TUSHARSHARMA0107 <tushar43189@gmail.com>
2025-11-13 22:21:25 +0530

    chage

--- booking-frontend/src/config/api.js
+++ booking-frontend/src/config/api.js
@@ -1,16 +1,15 @@
 //choose backend url accordingly
+let API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
 
-let API_BASE_URL=import.meta.env.VITE_API_BASE_URL||"http://localHost:5000/api";
-
-if (window.location.hostname=="localhost"){
-      const API_BASE_URL=import.meta.env.VITE_API_BASE_URL||"http://localHost:5000/api";
-}else if(window.location.hostname.includes("staging"))
-      {
-      const API_BASE_URL="https://staging.yourapp.com/api";
-}else{
-      API_BASE_URL="https://ypurapp.vercel.app/api"
+// Detect environment
+if (window.location.hostname === "localhost") {
+  API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
+} 
+else if (window.location.hostname.includes("staging")) {
+  API_BASE_URL = "https://staging.yourapp.com/api";
+} 
+else {
+  API_BASE_URL = "https://yourapp.vercel.app/api";
 }
-
-API_BASE_URL=import.meta.env.VITE_API_BASE_URL||API_BASE_URL;
 
 export default API_BASE_URL;
